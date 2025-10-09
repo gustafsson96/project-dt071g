8432e17cc6d8954b1b4a47187e73dae9e397b6eb
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-10-09 19:47:21 -0400

    Add questions table to database

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace quizApp
         public static void Main()
         {
             WriteLine("Welcome to my quiz app!");
-            databaseConnection.TestConnection();
+            databaseConnection.InitializeDatabase();
         }
     }
 }
--- databaseConnection.cs
+++ databaseConnection.cs
@@ -5,36 +5,42 @@ namespace quizApp
 {
     public class databaseConnection
     {
-        public static void TestConnection()
+        public static void InitializeDatabase()
         {
-            /*
-    var sql = @"CREATE TABLE questions(
-        id INTEGER PRIMARY KEY,
-        question TEXT NOT NULL,
-        first_alternative TEXT NOT NULL,
-        second_alternative TEXT NOT NULL,
-        third_alternative TEXT NOT NULL,
-        fourth_alternative TEXT NOT NULL
-    )";
-    */
+
+            var sql = @"CREATE TABLE IF NOT EXISTS questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category TEXT NOT NULL,
+            question TEXT NOT NULL,
+            first_alternative TEXT NOT NULL,
+            second_alternative TEXT NOT NULL,
+            third_alternative TEXT NOT NULL,
+            fourth_alternative TEXT NOT NULL,
+            correct_alternative INTEGER NOT NULL
+        )";
+
 
             try
             {
                 using var connection = new SqliteConnection("Data Source=quiz.db");
                 connection.Open();
 
-                /*
+                WriteLine("Connected to the SQLite database!");
+
                 using var command = new SqliteCommand(sql, connection);
                 command.ExecuteNonQuery();
                 WriteLine("Table questions was created successfully!");
-                */
 
-                WriteLine("Connected to the SQLite database!");
             }
             catch (SqliteException e)
             {
                 WriteLine(e.Message);
             }
+        }
+
+        public static void InsertQuestion()
+        {
+            
         }
     }
 }
