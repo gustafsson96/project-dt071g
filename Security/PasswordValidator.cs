5d32416575eaf6750bfe7507867441c67085908e
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-10-28 08:48:22 -0400

    Update comments for files in Security folder

--- Security/PasswordValidator.cs
+++ Security/PasswordValidator.cs
@@ -1,9 +1,12 @@
 namespace QuizApp.Security
 {
+    // Validates password for developer access
     public static class PasswordValidator
     {
+        // Checks if input matches password
         public static bool Validate(string input)
         {
+            // Password is hardcoded for demonstrational purposes in this version of the app
             return input == "password123";
         }
     }
--- Security/RequestAccess.cs
+++ Security/RequestAccess.cs
@@ -2,8 +2,10 @@ using static System.Console;
 
 namespace QuizApp.Security
 {
+    // Handles access for developer view
     public class DeveloperAccess
     {
+        // Prompts the user for a developer password before displaying developer menu
         public bool RequestAccess()
         {
             Clear();
@@ -12,7 +14,10 @@ namespace QuizApp.Security
                 Write("Insert developer password (or press 'q' to go back): ");
                 string input = ReadLine()!;
 
+                // Returns false if user quits and returns to main menu
                 if (input.ToLower() == "q") return false;
+
+                // Returns true if password is correct
                 if (PasswordValidator.Validate(input)) return true;
 
                 WriteLine("Access denied. Try again.");
